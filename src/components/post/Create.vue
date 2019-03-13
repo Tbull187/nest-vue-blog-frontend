0a31e9e34b006120fe55b679906970100f436f05
Tristan <tristan.tbull@gmail.com>
2019-03-13 16:57:11 -0700

    a few ui enhancements, creating NavBar component

--- src/components/post/Create.vue
+++ src/components/post/Create.vue
@@ -1,6 +1,6 @@
 <template>
    <div>
-        <div class="col-md-12 form-wrapper">
+        <div class="col-md-6 form-wrapper">
           <h2> Create Post </h2>
           <form id="create-post-form" @submit.prevent="createPost">
                <div class="form-group col-md-12">
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,39 +2,45 @@
     <div>
 
       <div class="text-center">
-        <h1>Nest Blog Tutorial</h1>
-       <p> This is the description of the blog built with Nest.js, Vue.js and MongoDB</p>
+        <h1>NestJS Blog</h1>
+       <p>A blog website built with Nest.js | Vue.js | MongoDB</p>
 
        <div v-if="posts.length === 0">
             <h2> No post found at the moment </h2>
         </div>
       </div>
 
-        <div class="row">
-           <div class="col-md-4" v-for="post in posts" :key="post._id">
-              <div class="card mb-4 shadow-sm">
-                <div class="card-body">
-                   <h2 class="card-img-top">{{ post.title }}</h2>
-                  <p class="card-text">{{ post.body }}</p>
-                  <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group" style="margin-bottom: 20px;">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4" v-for="post in posts" :key="post._id">
+                <div class="card mb-4 shadow-sm">
+                  <div class="card-body">
+                    <h2 class="card-img-top">{{ post.title }}</h2>
+                    <p class="card-text">{{ post.body }}</p>
+                    <div class="btn-group">
                       <router-link :to="{name: 'Post', params: {id: post._id}}" class="btn btn-sm btn-outline-secondary">View Post </router-link>
-                       <router-link :to="{name: 'Edit', params: {id: post._id}}" class="btn btn-sm btn-outline-secondary">Edit Post </router-link>
-                       <button class="btn btn-sm btn-outline-secondary" v-on:click="deletePost(post._id)">Delete Post</button>
+                      <router-link :to="{name: 'Edit', params: {id: post._id}}" class="btn btn-sm btn-outline-secondary">Edit Post </router-link>
+                      <button class="btn btn-sm btn-outline-secondary" v-on:click="deletePost(post._id)">Delete Post</button>
                     </div>
-                  </div>
 
-                  <div class="card-footer">
-                    <small class="text-muted">Posted on: {{ post.date_posted}}</small><br/>
-                    <small class="text-muted">by: {{ post.author}}</small>
-                  </div>
+                    <div class="card-footer">
+                      <small class="text-muted">Posted on: {{ post.date_posted }}</small><br/>
+                      <small class="text-muted">by: {{ post.author }}</small>
+                    </div>
 
+                  </div>
                 </div>
               </div>
-            </div>
-      </div>
+          </div>
+        </div>
     </div>
 </template>
+
+<style>
+.btn-group {
+  margin-bottom: 20px;
+}
+</style>
 
 <script>
 // @ is an alias to /src
